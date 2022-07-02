657ba49c58f50aa30c30317627adde6a7f663c21
pavel-rud <pahaprimorye2015@gmail.com>
2022-07-03 00:03:39 +1000

    telegram bot

---
+++ mysite/telegram_bot/main_bot.py
@@ -0,0 +1,121 @@
+import telebot
+import json
+from telebot import types
+from services_bot import *
+
+link = "http://127.0.0.1:8000"
+
+TOKEN = '2032582622:AAEkc3HyRv5YgsqqkCn1ioX8Cp86Rw26P_s'
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def command_help(message):
+    user_tg = message.from_user.username
+    create_user_tg(user_tg)
+    if tg_tied(user_tg):  # если пользователь привязал телеграмм
+        info = get_all_info(user_tg)
+        username = get_user_name(user_tg)
+        bot.send_message(message.chat.id, f"Привет, этот аккаунт телеграмма привязан "
+        f"к профилю {username} на нашем сайте😌\n"
+        f"Введите ID комнаты в которой вы хотите добавить видео/музыку в очередь\n"
+        f"/help - список команд")
+    else:
+        markup = types.InlineKeyboardMarkup()
+        button1 = types.InlineKeyboardButton("Наш сайт", url=link)
+        markup.add(button1)
+        bot.send_message(message.chat.id, f"Привет, этот аккаунт телеграмма не привязан "
+        f"к какому-либо профилю на нашем сайте😢\nНо вы всё равно можете пользоваться ботом😉"
+        f" Добавленные вами видео/музыка в очереди будут подписаны вашим телеграммом\n"
+        f"Если вы всё-таки хотите привязать свой телеграмм, то зарегистрируйтесь на сайте, если вы это ещё не сделали."
+        f"Введите ID комнаты в которой вы хотите добавить видео/музыку в очередь\n"
+        f"/help - список команд", reply_markup=markup)
+
+
+@bot.message_handler(commands=['help'])
+def command_help(message):
+    bot.send_message(message.chat.id, "Cписок команд:\n/leave_room - покинуть комнату\n"
+                                      "/state - вывести всю информацию на данный момент")
+
+
+@bot.message_handler(commands=['leave_room'])
+def leave_room_command(message):
+    user_tg = message.from_user.username
+    if not is_password_tied(user_tg):
+        leave_room(user_tg)
+        bot.send_message(message.chat.id, "Вы и так не находитесь в какой-либе комнате")
+    else:
+        leave_room(user_tg)
+        bot.send_message(message.chat.id, "Вы вышли из комнаты")
+
+
+@bot.message_handler(commands=['state'])
+def state_command(message):
+    user_tg = message.from_user.username
+    if tg_tied(user_tg):  # если пользователь привязал телеграмм
+        info = get_all_info(user_tg)
+        username = get_user_name(user_tg)
+        bot.send_message(message.chat.id, f"Этот аккаунт телеграмма привязан "
+        f"к профилю {username} на нашем сайте😌")
+    else:
+        info = get_all_info(user_tg)
+        bot.send_message(message.chat.id,  f"Этот аккаунт телеграмма не привязан "
+        f"к какому-либо профилю на нашем сайте😢")
+    if info[3]:
+        info = get_all_info(user_tg)
+        room_info = get_info_about_room(user_tg, info[2])
+        owner = get_owner(room_info[3])
+        is_music = "Музыкальная" if room_info[2] else "Видеокомната"
+        bot.send_message(message.chat.id, f"Вы находитесь в комнате с id {info[2]}\n"
+        f"Название комнаты: {room_info[1]}\n"
+        f"Владелец комнаты: {owner}\n"
+        f"Тип комнаты: {is_music}")
+    elif info[2]:
+        bot.send_message(message.chat.id, f"Чтоб войти в комнату с id {info[2]} введите пароль")
+    else:
+        bot.send_message(message.chat.id, f"Введите ID комнаты в которой вы хотите добавить видео/музыку в очередь")
+
+
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    chat_id = message.chat.id
+    user_tg = message.from_user.username
+    if is_password_tied(user_tg):
+        # link
+        if add_link(user_tg, message.text):
+            bot.send_message(chat_id, "Вы успешно добавили в очередь!")
+        else:
+            bot.send_message(chat_id, "Что-то пошло не так, попробуйте ещё раз...")
+    elif is_room_tied(user_tg):
+        # password
+        if password_is_valid(user_tg, message.text):
+            info = get_all_info(user_tg)
+            room_info = get_info_about_room(user_tg, info[2])
+            owner = get_owner(room_info[3])
+            is_music = "Музыкальная" if room_info[2] else "Видеокомната"
+            bot.send_message(chat_id, f"Вы успешно присоеднились к комнате c id {info[2]}😌\n"
+            f"Название комнаты: {room_info[1]}\n"
+            f"Владелец комнаты: {owner}\n"
+            f"Тип комнаты: {is_music}")
+        else:
+            info = get_all_info(user_tg)
+            bot.send_message(chat_id, f"Неверный пароль для комнаты с id {info[2]}, попробуйте ещё раз")
+        bot.delete_message(chat_id, message.message_id)
+    else:
+        # room
+        if room_exist(user_tg, message.text):
+            bot.send_message(chat_id, f"Введите пароль для входа в комнату с id {message.text}")
+        else:
+            bot.send_message(chat_id, f"Комната с таким id {message.text} не найдена")
+
+
+def run_bot():
+    try:
+        print("TelegramBot is ready")
+        bot.infinity_polling()
+    except Exception as error:
+        print(error)
+
+
+run_bot()
---
+++ mysite/telegram_bot/services_bot.py
@@ -0,0 +1,245 @@
+import sqlite3
+import requests
+import json
+
+
+mainlink = "http://127.0.0.1:8000"
+
+
+def get_info_about_room(tg_id, room_id):
+    try:
+        response = requests.get(f"{mainlink}/api/get_rooms/").json()
+        room = [i for i in list(response) if str(i["id"])==str(room_id)][0]
+        owner_room_id = room["owner_id"]
+        res = [room["id"], room["name_room"], room["is_music"], owner_room_id]
+        print(room)
+        return res
+    except:
+        res = ['', '', '', '']
+        return res
+
+
+def leave_room(tg_id):
+    try:
+        con = sqlite3.connect("botDB.db")
+        cur = con.cursor()
+        cur.execute('''
+                  CREATE TABLE IF NOT EXISTS users (tg_id         STRING UNIQUE, user_id       STRING, room_id       STRING,password_room STRING)''')
+        cur.execute(
+            f"""UPDATE users SET room_id=Null, password_room=Null
+                     where tg_id='{tg_id}' """)
+        con.commit()
+        con.close()
+        return True
+    except:
+        return False
+
+
+def add_room_id(tg_id, room_id):
+    try:
+        con = sqlite3.connect("botDB.db")
+        cur = con.cursor()
+        cur.execute('''
+                  CREATE TABLE IF NOT EXISTS users (tg_id         STRING UNIQUE, user_id       STRING, room_id       STRING,password_room STRING)''')
+        cur.execute(
+            f"""UPDATE users SET room_id='{room_id}'
+                     where tg_id='{tg_id}' """)
+        con.commit()
+        con.close()
+        return True
+    except:
+        return False
+
+
+def add_password_room(tg_id, password):
+    try:
+        con = sqlite3.connect("botDB.db")
+        cur = con.cursor()
+        cur.execute('''
+                  CREATE TABLE IF NOT EXISTS users (tg_id         STRING UNIQUE, user_id       STRING, room_id       STRING,password_room STRING)''')
+        cur.execute(
+            f"""UPDATE users SET password_room='{password}'
+                     where tg_id='{tg_id}' """)
+        con.commit()
+        con.close()
+        return True
+    except:
+        return False
+
+
+def add_user_id(tg_id, user_id):
+    try:
+        con = sqlite3.connect("botDB.db")
+        cur = con.cursor()
+        cur.execute('''
+                  CREATE TABLE IF NOT EXISTS users (tg_id         STRING UNIQUE, user_id       STRING, room_id       STRING,password_room STRING)''')
+        cur.execute(
+            f"""UPDATE users SET user_id='{user_id}'
+                     where tg_id='{tg_id}' """)
+        con.commit()
+        con.close()
+        return True
+    except:
+        return False
+
+
+def add_password(tg_id, password):
+    try:
+        con = sqlite3.connect("botDB.db")
+        cur = con.cursor()
+        cur.execute('''
+                  CREATE TABLE IF NOT EXISTS users (tg_id         STRING UNIQUE, user_id       STRING, room_id       STRING,password_room STRING)''')
+        cur.execute(
+            f"""UPDATE users SET password_room='{password}'
+                     where tg_id='{tg_id}' """)
+        con.commit()
+        con.close()
+        return True
+    except:
+        return False
+
+
+def create_user_tg(tg_id):
+    try:
+        con = sqlite3.connect("botDB.db")
+        cur = con.cursor()
+        cur.execute('''
+                  CREATE TABLE IF NOT EXISTS users (tg_id         STRING UNIQUE, user_id       STRING, room_id       STRING,password_room STRING)''')
+        cur.execute(
+            f"""INSERT INTO users  (tg_id) VALUES('{tg_id}')""")
+        con.commit()
+        con.close()
+        return True
+    except:
+        return False
+
+
+def is_room_tied(tg_id):
+    try:
+        con = sqlite3.connect("botDB.db")
+        cursor = con.cursor()
+        cursor.execute('''
+                  CREATE TABLE IF NOT EXISTS users (tg_id         STRING UNIQUE, user_id       STRING, room_id       STRING,password_room STRING)''')
+        cursor.execute(f"""SELECT room_id FROM users where tg_id='{tg_id}'""")
+        room = cursor.fetchall()[0][0]
+        con.commit()
+        con.close()
+        return True if room else False
+    except:
+        return False
+
+
+def get_all_info(tg_id):
+    try:
+        con = sqlite3.connect("botDB.db")
+        cursor = con.cursor()
+        cursor.execute('''
+                  CREATE TABLE IF NOT EXISTS users (tg_id         STRING UNIQUE, user_id       STRING, room_id       STRING,password_room STRING)''')
+        cursor.execute(f"""SELECT * FROM users where tg_id='{tg_id}'""")
+        room = cursor.fetchall()[0]
+        con.commit()
+        con.close()
+        return room
+    except:
+        return ['', '', '','']
+
+
+def is_password_tied(tg_id):
+    try:
+        con = sqlite3.connect("botDB.db")
+        cursor = con.cursor()
+        cursor.execute('''
+                  CREATE TABLE IF NOT EXISTS users (tg_id         STRING UNIQUE, user_id       STRING, room_id       STRING,password_room STRING)''')
+        cursor.execute(f"""SELECT password_room FROM users where tg_id='{tg_id}'""")
+        password = cursor.fetchall()[0][0]
+        con.commit()
+        con.close()
+        return True if password else False
+    except:
+        return False
+
+# api
+
+
+def get_user_name(tg_id):
+    try:
+        response = requests.get(f"{mainlink}/api/get_users/").json()
+        for i in response:
+            print(i, str(i['tg_name']), str(tg_id))
+            if str(i['tg_name']) == str(tg_id):
+                return i['username']
+        return ''
+    except:
+        return ''
+
+
+def get_owner(id):
+    try:
+        response = requests.get(f"{mainlink}/api/get_users/").json()
+        for i in response:
+            if str(i['id']) == str(id):
+                return i['username']
+        return ''
+    except:
+        return ''
+
+
+def tg_tied(tg_id):
+    try:
+        response = requests.get(f"{mainlink}/api/get_users/").json()
+        for i in response:
+            if str(i['tg_name']) == str(tg_id):
+                add_user_id(tg_id, i['id'])
+                return True
+        return False
+    except:
+        return False
+
+
+def password_is_valid(tg_id, enter_password):
+    try:
+        room_id = get_all_info(tg_id)[2]
+        #  print(room_id)
+        response = requests.get(f"{mainlink}/api/get_rooms/").json()
+        password_room = [i["password_room"] for i in list(response) if i["id"] == room_id][0]
+        #  print(password_room)
+        if password_room == enter_password:
+            add_password(tg_id, enter_password)
+            return True
+        return False
+    except:
+        return False
+
+
+def room_exist(tg_id, room_id):
+    try:
+        #  print(room_id)
+        response = requests.get(f"{mainlink}/api/get_rooms/").json()
+        all_rooms_id = [str(i["id"]) for i in list(response)]
+        #  print(all_rooms_id)
+
+        if room_id in all_rooms_id:
+            add_room_id(tg_id, room_id)
+            return True
+        return False
+    except:
+        return False
+
+
+def add_link(tg_id, link):
+    try:
+        info = get_all_info(tg_id)
+        #  print(info)
+        # (link=str, room=id_room, tg_name=str, password=password)
+        json_link = json.dumps({'link': str(link), 'room': int(info[2]),
+                     'tg_name': str(tg_id), 'password': str(info[3])})
+
+        response = requests.post(f"{mainlink}/api/new_link/", json=json_link)
+        #
+        print(json_link)
+        print({'link': str(link), 'room': int(info[2]),
+                                       'tg_name': str(tg_id), 'password': str(info[3])})
+        return True
+    except Exception as error:
+        print(error)
+        return False
